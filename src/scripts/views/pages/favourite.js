e2ee7d0e2328a2bb44ceb09eba2d059f993fdcd6
rianaratna <riana06huan18@gmail.com>
2023-11-07 11:04:09 +0700

    add error message template, make some code changes, and delete comments

--- src/scripts/views/pages/favourite.js
+++ src/scripts/views/pages/favourite.js
@@ -1,18 +1,20 @@
 import FavouriteRestaurantIdb from '../../data/favourite-restaurant-idb';
+import { createErrorMessageTemplate } from '../templates/template-creator';
 
 // Components
 import '../../component/main/restaurant-list';
+import '../../component/main/restaurant-item';
 import '../../component/loading-indicator';
 
 const Favourite = {
     async render() {
         return `
             <div class="content favouriteSection">
+                <loading-indicator></loading-indicator>
                 <h2 class="secondaryTitle favouriteTitle" tabindex="0">Favourite</h2>
                 <div id="favourites" class="favourites">
                     <restaurant-list></restaurant-list>
                 </div>
-                <loading-indicator></loading-indicator>
             </div>
         `;
     },
@@ -25,31 +27,16 @@ const Favourite = {
                 const results = await FavouriteRestaurantIdb.getAllRestaurant();
                 $('loading-indicator').css({ display: 'none' });
                 if (results.length === 0) {
-                    const emptyFavouriteListMessage = `
-                        <style>
-                            .emptyFavouriteMessage {
-                                text-align: center;
-                                font-size: clamp(1.5em, calc(3vw + 1em), 3.125em); 
-                                color: black;
-                            }
-                        </style>
-                        <p class="emptyFavouriteMessage">You don't have favourite restaurant</p>
+                    document.querySelector('#favourites').innerHTML = `
+                        <p class="emptyFavouriteMessage" tabindex="0">You don't have favourite restaurant</p>
                     `;
-                    $('#favourites').innerHTML = emptyFavouriteListMessage;
                 } else {
                     const restaurantList = document.querySelector('restaurant-list');
                     restaurantList.restaurants = results;
-                    console.log(restaurantList);
-
-                    // results.forEach((result) => {
-                    //     const restaurantItem = document.querySelector('restaurant-item');
-                    //     restaurantItem.restaurant = result;
-                    //     console.log(restaurantItem);
-                    //     $('#favourites').innerHTML += restaurantItem;
-                    // });
+                    $('.secondaryTitle.restaurantTitle').css({ display: 'none' });
                 }
             } catch (error) {
-                console.error('Error in favourite page...', error);
+                $('.content').innerHTML = createErrorMessageTemplate('The request is failed. Please reload the page');
             }
         }, 2000);
     },
