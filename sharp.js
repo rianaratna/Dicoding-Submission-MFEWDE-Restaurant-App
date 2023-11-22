8a9b187d3c7cedbbc6efdf38beae677132ad82b4
rianaratna <riana06huan18@gmail.com>
2023-11-22 10:09:21 +0700

    Revert "Resolve some issue from hero image responsiveness"
    This reverts commit 6afad4d12f0126c29fc5ba03ee290455bc54dceb.

--- sharp.js
+++ sharp.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // Root Images
 const target = path.resolve(__dirname, 'src/public/images/heros');
-const destination = path.resolve(__dirname, 'src/public/images/heros');
+const destination = path.resolve(__dirname, 'dist/images');
 
 if (!fs.existsSync(destination)) {
     fs.mkdirSync(destination);
@@ -12,21 +12,24 @@ if (!fs.existsSync(destination)) {
 
 fs.readdirSync(target)
     .forEach((image) => {
-        sharp(`${target}/${image}`)
-            .resize(1200)
-            .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
-                .slice(0, -1)
-                .join('.')}-large.jpg`));
+        const extension = image.split('.')[1] === 'png' ? 'png' : 'jpg';
+        if (image.includes('hero-image_4')) {
+            sharp(`${target}/${image}`)
+                .resize(1200)
+                .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
+                    .slice(0, -1)
+                    .join('.')}-large.${extension}`));
 
-        sharp(`${target}/${image}`)
-            .resize(800)
-            .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
-                .slice(0, -1)
-                .join('.')}-medium.jpg`));
+            sharp(`${target}/${image}`)
+                .resize(800)
+                .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
+                    .slice(0, -1)
+                    .join('.')}-medium.${extension}`));
 
-        sharp(`${target}/${image}`)
-            .resize(600)
-            .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
-                .slice(0, -1)
-                .join('.')}-small.jpg`));
+            sharp(`${target}/${image}`)
+                .resize(600)
+                .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
+                    .slice(0, -1)
+                    .join('.')}-small.${extension}`));
+        }
     });
