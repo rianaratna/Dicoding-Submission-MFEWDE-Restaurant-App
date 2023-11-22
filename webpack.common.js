6afad4d12f0126c29fc5ba03ee290455bc54dceb
rianaratna <riana06huan18@gmail.com>
2023-11-22 10:02:17 +0700

    Resolve some issue from hero image responsiveness

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    assetModuleFilename: 'assets/[name].[contenthash][ext]',
+    assetModuleFilename: 'images/[name].[contenthash][ext]',
     clean: true,
   },
   optimization: {
